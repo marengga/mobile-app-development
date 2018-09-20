00a5aff4a2a3056b72b74bf73d4bf505afdf5a27
Andri K. Marengga <andry.marengga@visionet.co.id>
2018-09-20 19:27:14 +0700

    Day 2 - Variable!

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
+var kalimatlama = "Halo lama!";
+const kalimat1 = "Halo Dunia 1!";
+let kalimat2 = "Halo Dunia 2!"
 export default class App extends Component {
   render() {
     return (
-        <Text>
-          Hello world!
+      <View>
+        <Text style={{ marginTop: 100 }}>
+          {kalimatlama}
         </Text>
+        <Text style={{ marginTop: 100 }}>
+          {kalimat1}
+        </Text>
+        <Text style={{ marginTop: 100 }}>
+          {kalimat2}
+        </Text>
+      </View>
     );
   }
 }
