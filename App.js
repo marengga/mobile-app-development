1cb452494c26fe8761c1a7571c1511716d72a9c9
Andri K. Marengga <andry.marengga@visionet.co.id>
2018-09-20 19:30:45 +0700

    Day 2 - Constructor

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-var kalimatlama = "Halo lama!";
-const kalimat1 = "Halo Dunia 1!";
-let kalimat2 = "Halo Dunia 2!"
 export default class App extends Component {
-  render() {
+  constructor(){
+    super();
+    this.kalimat = "Kalimat dari constructor!";
+  }
+
+  render() {  
     return (
       <View>
         <Text style={{ marginTop: 100 }}>
-          {kalimatlama}
-        </Text>
-        <Text style={{ marginTop: 100 }}>
-          {kalimat1}
-        </Text>
-        <Text style={{ marginTop: 100 }}>
-          {kalimat2}
+          {this.kalimat}
         </Text>
       </View>
     );
