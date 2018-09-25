14200a9c214ec51bbd01170f280e1e00ac4959fd
Andri K. Marengga <andry.marengga@visionet.co.id>
2018-09-25 14:53:43 +0700

    Day 2 - StyleSheet

--- App.js
+++ App.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{ marginTop: 100 }}>
-        <Text style={{
-          fontSize:50,
-          color:'red'
-        }}>
-          Darah itu merah!
-        </Text>
-        
-        <Text style={{
-          fontSize:40,
-          fontStyle:'italic',
-          fontWeight:'bold',
-          textAlign:'center',
-          color:'yellow',
-          backgroundColor:'blue',
-          marginVertical:20,
-          marginHorizontal:15
-        }}>
+        <Text style={style.warnawarni}>
           Life is so colorful!
         </Text>
       </View>
     );
   }
 }
+
+const style = StyleSheet.create({
+  warnawarni: {
+    fontSize: 40,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'yellow',
+    backgroundColor: 'blue',
+    marginVertical: 20,
+    marginHorizontal: 15
+  }
+})
