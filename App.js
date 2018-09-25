80364ade70aa695d1811db7fcac16bb783d5f7da
Andri K. Marengga <andry.marengga@visionet.co.id>
2018-09-25 15:11:27 +0700

    Day 2 - State

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Header from './src/components/Header';
-import Footer from './src/components/Footer';
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Header from "./src/components/Header";
+import Footer from "./src/components/Footer";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nama: "Vincent",
+      usia: 15
+    };
+  }
+
   render() {
     return (
-      <View style={{marginTop:100}}>
+      <View style={{ marginTop: 100 }}>
         <Header />
 
-        <Text>Text di App.js</Text>
-        <Text>Ini juga</Text>
-        <Text>Ini juga sama</Text>
+        <Text style={{fontSize:50}}>Halo {this.state.nama}!</Text>
+        <Text style={{fontSize:50}}>Usia kamu saat ini {this.state.usia} tahun ya?</Text>
 
         <Footer />
       </View>
