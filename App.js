d3404238121eb8fbf8d98fa24a84fc8c3380fafe
Andri K. Marengga <andry.marengga@visionet.co.id>
2018-09-25 14:40:29 +0700

    Day 2 - Function Expression

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 export default class App extends Component {
-  kali(a,b){
-    return a*b;
-  }
-  render() {  
+  render() {
+    function kali(a,b){
+      return a*b;
+    }
+
     return (
       <View>
         <Text style={{ marginTop: 100 }}>
-          Hitung 15 x 20 = {this.kali(15,20)}
+          Hitungin 12 x 34 = {kali(12, 34)}
         </Text>
       </View>
     );
