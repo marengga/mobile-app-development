966170f9b5b2dcefee8876aa0346825f43cc99dc
Andri K. Marengga <andry.marengga@visionet.co.id>
2018-09-25 15:07:14 +0700

    Day 2 - Export Import Custom Components

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Header from './src/components/Header';
+import Footer from './src/components/Footer';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <View style={{ flexDirection: 'row', marginTop: 50 }}>
-          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-          <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
-          <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
-        </View>
+      <View style={{marginTop:100}}>
+        <Header />
 
-        <View style={{ flexDirection: 'column', justifyContent: 'space-between', marginTop: 50, height: 200, backgroundColor: '#eee' }}>
-          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-          <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
-          <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
-        </View>
+        <Text>Text di App.js</Text>
+        <Text>Ini juga</Text>
+        <Text>Ini juga sama</Text>
 
-        <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'stretch', marginTop:50 }}>
-          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
-          <View style={{ height: 50, backgroundColor: 'skyblue' }} />
-          <View style={{ height: 100, backgroundColor: 'steelblue' }} />
-        </View>
+        <Footer />
       </View>
     );
   }
